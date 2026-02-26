a0a93113852993a0b589408141a84cca95854975
borared <boraofficial76@gmail.com>
2026-02-26 15:20:44 +0700

    Finish Step 2: CREATE THE SCORE COMPONENT

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,16 @@
 import Header from "./components/Header";
+import { JAVA_RESULTS, PYTHON_RESULTS, HTML_RESULTS, ENGLISH_RESULTS } from "./data";
+import Score from "./components/Score.jsx";
 function App() {
   return (
     <>
       <Header/>
 
       <main className="scores-container">
-        <div class="scores">
-          <h1>Fake Course</h1>
-
-          <table>
-            <thead>
-              <tr>
-                <th>First name</th>
-                <th>Last name</th>
-                <th>Score</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>First name 1 </td>
-                <td>Last name 1 </td>
-                <td>55</td>
-              </tr>
-              <tr>
-                <td>First name 2 </td>
-                <td>Last name 2 </td>
-                <td>45</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+        <Score courseName="Java" courseResults={JAVA_RESULTS}></Score>
+        <Score courseName="HTML" courseResults={HTML_RESULTS}></Score>
+        <Score courseName="Python" courseResults={PYTHON_RESULTS}></Score>
+        <Score courseName="English" courseResults={ENGLISH_RESULTS}></Score>
       </main>
     </>
   );
